758f0461acdcb3f545cef2027292c240999bff1d
orbitalquark <70453897+orbitalquark@users.noreply.github.com>
2025-01-01 10:19:27 -0500

    Updated copyright year.

--- gdb/init_test.lua
+++ gdb/init_test.lua
@@ -1,4 +1,4 @@
--- Copyright 2020-2024 Mitchell. See LICENSE.
+-- Copyright 2020-2025 Mitchell. See LICENSE.
 
 local debugger = require('debugger')
 require('debugger.gdb').logger = test.log
--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- Copyright 2007-2024 Mitchell. See LICENSE.
+-- Copyright 2007-2025 Mitchell. See LICENSE.
 
 --- Language debugging support for Textadept.
 --
